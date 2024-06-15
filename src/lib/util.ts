f5f4a4e9987f12954d3370ea44a194cd1b972f8b
Julian Fortune <juliandfortune@gmail.com>
2024-06-15 16:23:16 -0700

    Tweaks to `util.ts`

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -2,8 +2,8 @@ export function toIndexMap<T>(arr: Array<T>): Map<number, T> {
     return new Map((arr.map((value, index) => [index, value])))
 }
 
-// Utilities for working with (key, value) maps
 export const mapToArray = <K, A>(map: Map<K, A>) => [...map.entries()]
+
 export const valuesToArray = <K, A>(map: Map<K, A>) => [...map.values()]
 
 export const objToMap = <A>(o: { [s: string]: A; }) => new Map(Object.entries(o))
@@ -17,14 +17,14 @@ export function getRandomEntry<K, V>(map: Map<K, V>): [K, V] {
 }
 
 export function getRandomElement<A>(arr: Array<A>): A {
-    if (arr.length == 0) { throw Error("Map must not be empty!") }
+    if (arr.length == 0) { throw Error("Array must not be empty!") }
 
     const randomIndex = Math.floor(Math.random() * arr.length);
     return arr[randomIndex];
 }
 
 export function getRandomElementWithIndex<A>(arr: Array<A>): [A, number] {
-    if (arr.length == 0) { throw Error("Map must not be empty!") }
+    if (arr.length == 0) { throw Error("Array must not be empty!") }
 
     const randomIndex = Math.floor(Math.random() * arr.length);
     return [arr[randomIndex], randomIndex];
